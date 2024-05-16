4fce87dfcdd1b7ed5c6aa90e9aa53ed2078e5072
jaewon <app235naver@gmail.com>
2024-05-16 13:24:35 +0900

    feat 검색 및 판매수 크롤

---
+++ search_from_naver_shopping.py
@@ -0,0 +1,102 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from openpyxl import load_workbook
+from time import sleep
+
+# 엑셀 파일 로드
+workbook = load_workbook(filename="brand.xlsx")
+
+sheet = workbook.active
+
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+}
+
+chrome_options = Options()
+#브라우저 꺼짐 방지
+chrome_options.add_experimental_option("detach", True)
+#불필요한 에러 메시지 없애기
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"]) # 셀레니움 로그 무시
+
+chrome_options.add_argument(r'load-extension=C:\Users\jaewon\AppData\Local\Google\Chrome Dev\User Data\Profile 1\Extensions\cgococegfcmmfcjggpgelfbjkkncclkf\1.1.9.3_0')
+
+service = webdriver.ChromeService(executable_path='C:\Program Files\Google\Chrome Dev\Application\chrome.exe')
+
+
+find_site = ""
+
+
+if __name__ == '__main__':
+    
+    b = webdriver.Chrome(options=chrome_options)
+    
+    max_row_cnt = sheet.max_row
+    
+    for i in range(1643, max_row_cnt - 1):
+        
+        keyword = sheet[f'A{i}'].value
+        
+        b.get(f"https://search.shopping.naver.com/search/all?query={keyword.strip()}")
+        # b.get(f"https://search.shopping.naver.com/search/all?query=자전거")
+        
+        
+        iframe = b.find_element(By.CSS_SELECTOR, 'iframe#sellerlife-keyword-analysis-frame')
+        b.switch_to.frame(iframe)
+        
+        # 렌더링 타이머
+        sleep(3)
+        
+        tt = b.find_element(By.CLASS_NAME, "totalprdcnt")
+        총상품수 = int(b.find_element(By.CLASS_NAME, "cnt").text.strip().replace(",", "").replace("개", ""))
+        
+        gg = b.find_element(By.CLASS_NAME, "ovsprdcnt")
+        해외상품수 = int(gg.find_element(By.CLASS_NAME, "cnt").text.strip().replace(",", "").replace("개", ""))
+        
+        판매건수 = b.find_elements(By.XPATH, "//*[contains(@class,'sellcount')]")
+        
+        top_40_6개월_판매건수 = int(판매건수[0].text.strip().replace(",", "").replace("개", ""))
+        top_80_6개월_판매건수 = int(판매건수[1].text.strip().replace(",", "").replace("개", ""))
+        
+        매출액 = b.find_elements(By.XPATH, "//*[contains(@class,'revenue')]")
+        
+        top_40_6개월_매출액 = int(매출액[0].text.strip().replace(",", "").replace("원", ""))
+        top_80_6개월_매출액 = int(매출액[1].text.strip().replace(",", "").replace("원", ""))
+        
+        try:
+            검색량_최근1달 = int(b.find_element(By.XPATH, "//*[contains(@class,'monthlyQcCnt')]").text.strip().replace(",", ""))
+        except Exception as e:
+            검색량_최근1달 = b.find_element(By.XPATH, "//*[contains(@class,'monthlyQcCnt')]").text.strip()
+        
+        try:
+            검색량_예상1달 = int(b.find_element(By.XPATH, "//*[contains(@class,'estQcCnt')]").text.strip().replace(",", ""))
+        except Exception as e:
+            검색량_예상1달 = b.find_element(By.XPATH, "//*[contains(@class,'estQcCnt')]").text
+
+        try:        
+            검색량_예상3달평균 = int(b.find_element(By.XPATH, "//*[contains(@class,'estThrQcCnt')]").text.strip().replace(",", ""))
+        except Exception as e:
+            검색량_예상3달평균 = b.find_element(By.XPATH, "//*[contains(@class,'estQcCnt')]").text
+    
+        # ===========================클롱링 데이터 =================================
+        
+        
+
+        sheet.cell(row=i, column=3).value = 총상품수
+        sheet.cell(row=i, column=4).value = 해외상품수
+        sheet.cell(row=i, column=5).value = 검색량_최근1달
+        sheet.cell(row=i, column=6).value = 검색량_예상1달
+        sheet.cell(row=i, column=7).value = 검색량_예상3달평균
+        sheet.cell(row=i, column=8).value = top_40_6개월_판매건수
+        sheet.cell(row=i, column=9).value = top_80_6개월_판매건수
+        sheet.cell(row=i, column=10).value = top_40_6개월_매출액
+        sheet.cell(row=i, column=11).value = top_80_6개월_매출액
+        
+        
+        workbook.save(filename="brand.xlsx")    
+        
+    print("======끝==========")
+    b.quit()
+
